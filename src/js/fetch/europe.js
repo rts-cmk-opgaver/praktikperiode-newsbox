273edca5578b627d607d75acac362874f5fd0631
Kasper <77611053+kawern@users.noreply.github.com>
2021-05-04 12:13:53 +0200

    why cant i make this fetch shit work

--- src/js/fetch/europe.js
+++ src/js/fetch/europe.js
@@ -1,3 +1,5 @@
+let test = document.querySelector(".news__category-title");
+let test2 = document.querySelector("news__category-article")
 
 fetch("https://rss.nytimes.com/services/xml/rss/nyt/Europe.xml")
 .then(response => response.text())
@@ -7,4 +9,11 @@ fetch("https://rss.nytimes.com/services/xml/rss/nyt/Europe.xml")
     const srcDOM = parser.parseFromString(result, "application/xml"); // convert dom string to dom tree. 
 return xml2json(srcDOM)
 })
-.then(jsonResult => console.log(jsonResult.rss.channel.item))
+.then(jsonResult => 
+       test.innerHTML = `${jsonResult.rss.channel.title}`
+       )
+
+       .then(jsonResult =>
+        test2.innerHTML = `    <div class="news__category-article-img">${jsonResult.rss.channel.item.title}</div>
+        <div class="news__category-article-title">${jsonResult.rss.channel.item.title}</div>
+        <div class="news__category-article-content">${jsonResult.rss.channel.title}</div>`)
---
+++ src/js/fetch/xmlfetch.js
@@ -0,0 +1,34 @@
+/**
+ * This function coverts a DOM Tree into JavaScript Object. 
+ * @param srcDOM: DOM Tree to be converted. 
+ */
+ function xml2json(srcDOM) {
+    let children = [...srcDOM.children];
+  
+    // base case for recursion. 
+    if (!children.length) {
+      return srcDOM.innerHTML
+    }
+  
+    // initializing object to be returned. 
+    let jsonResult = {};
+  
+    for (let child of children) {
+  
+      // checking is child has siblings of same name. 
+      let childIsArray = children.filter(eachChild => eachChild.nodeName === child.nodeName).length > 1;
+  
+      // if child is array, save the values as array, else as strings. 
+      if (childIsArray) {
+        if (jsonResult[child.nodeName] === undefined) {
+          jsonResult[child.nodeName] = [xml2json(child)];
+        } else {
+          jsonResult[child.nodeName].push(xml2json(child));
+        }
+      } else {
+        jsonResult[child.nodeName] = xml2json(child);
+      }
+    }
+  
+    return jsonResult;
+  }
